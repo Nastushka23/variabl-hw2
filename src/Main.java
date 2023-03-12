9aa204493bc770748d6406be07112aa537bb5f28
user <89842713320@mail.ru>
2023-03-12 23:45:47 +0800

    Домашка сделана

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        var dog=8.0;
+        System.out.println(dog);
+        var cat=3.6;
+        System.out.println(cat);
+        var paper=763789;
+        System.out.println(paper);
+        dog=dog+4;
+        System.out.println(dog);
+        cat=cat+4;
+        System.out.println(cat);
+        paper=paper+4;
+        System.out.println(paper);
+        dog=dog-3.5;
+        System.out.println(dog);
+        cat=cat-1.6;
+        System.out.println(cat);
+        paper=paper-7639;
+        System.out.println(paper);
+        var friend=19;
+        System.out.println(friend);
+        friend=friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+
+        var weightOneBoxer=78.2;
+        var weightSecondBoxer=82.7;
+        var totalWeightBoxers=weightOneBoxer+weightSecondBoxer;
+        System.out.println("Общий вес бойцов "+totalWeightBoxers+" кг");
+        var differenceWeightBoxers=weightSecondBoxer-weightOneBoxer;
+        System.out.println("Разница между весами бойцов "+differenceWeightBoxers+" кг");
+        var ostatokWeight=weightSecondBoxer%weightOneBoxer;
+        System.out.println("Остаток "+ostatokWeight+" кг");
+
+        var workHours=640;
+        var employeeCompany=workHours/8;
+        System.out.println("Всего работников в компании " +employeeCompany+ " человек");
+        var allEmployee=employeeCompany+94;
+        var openingHours=allEmployee*8;
+        System.out.println("Если в компании работает "+allEmployee+" человек, то всего "+openingHours+ " часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
